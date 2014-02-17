e3393e278d983472b2090508fb5796ca18794965
Keith Bingman <keith@mobify.com>
2014-02-17 14:57:47 -0800

    returning the object method. I think this is correct...

--- commands/waitForPageToBeMobified.js
+++ commands/waitForPageToBeMobified.js
@@ -1,17 +1,12 @@
-exports.command = function(template, callback) {
+exports.command = function(callback) {
     var self = this;
     var start = new Date().getTime();
 
     // Uses the browser.execute to run code within the client browser,
     // access the Mobify object and test the template.
-    this.execute_async(
+    return this.execute_async(
         // gets the Mobify evaluatedDate object
-        function() {
-            var evaluatedData = Mobify.evaluatedData;
-            if (evaluatedData){
-                return evaluatedData;
-            }
-        },
+        function() { return Mobify.evaluatedData; },
         [],
         function(result) {
             var evaluatedData = result.value;
@@ -20,12 +15,15 @@ exports.command = function(template, callback) {
                 (now - start) + ' milliseconds.';
 
             // Let the DOM settle down after Mobify has done its stuff.
-            self.pause(2000);
+            // self.pause(2000);
 
             // Assert that the evaluatedData object is present
-            self.assert.notEqual(evaluatedData, undefined, msg)
+            self.assert.notEqual(evaluatedData, undefined, msg);
+
+            if (typeof callback === 'function') {
+                callback.call(self, result);
+            }
         }
     );
 
-    return this;
 };
