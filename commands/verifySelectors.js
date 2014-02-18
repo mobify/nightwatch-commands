afbdf4a0cbae439818f246fc531ff579712fd9a9
Steve Calvert <scalvert@mobify.com>
2014-02-17 17:06:52 -0800

    Updating verify selectors command. May get rid of this

--- commands/verifySelectors.js
+++ commands/verifySelectors.js
@@ -1,41 +1,35 @@
 var async = require('async');
 
 exports.command = function(selectors) {
-    var self = this,
-        url = '', 
+    var client = this,
         missing = [];
 
-    if (!Array.isArray(selectors)) {
-        selectors = Array.prototype.slice.call(arguments, 0);
-    }
+    selectors = Array.prototype.slice.call(arguments, 0);
 
-    async.series([
-        function(callback) {
-            self.url(function(result) {
-                url = result.value;
-                callback(null, result);
+    async.each(selectors,
+        function(selector, cb) {
+            client.element.call(client.client, 'css selector', selector, function(result) {
+                var value;
+
+                if (result.status == 0) {
+                    value = result.value.ELEMENT;
+                }
+
+                if (!value) {
+                    missing.push(selector);
+                }
+
+                cb();
             });
         },
-        function(callback) {
-            selectors.forEach(function(selector) {
-                self.element.call(self.client, 'css selector', selector, function(result) {
-                    var value;
+        function(err) {
+            if (err) {
+                client.info('An error occurred while verifying selectors');
+            } else {
+                client.assert(!missing.length, 'The following selectors are missing: ' + missing.join(', '));
+            }
 
-                    if (result.status == 0) {
-                        value = result.value.ELEMENT;
-                    }
-
-                    if (!value) {
-                        missing.push(selector);
-                    }
-
-                    callback(null, 'done');
-                });
-            });
-        }
-    ]);
-
-    self.assertion(!missing.length, missing, true, 'All selectors found', false);
+        });
 
     return this;
 };
