2dd02e6581338a347bdce5b0b05c13117d0e516c
Keith Bingman <keith@mobify.com>
2014-02-17 18:21:16 -0800

    very simple version of verify selectors

--- commands/verifySelectors.js
+++ commands/verifySelectors.js
@@ -4,32 +4,11 @@ exports.command = function(selectors) {
     var client = this,
         missing = [];
 
-    selectors = Array.prototype.slice.call(arguments, 0);
+    // selectors = Array.prototype.slice.call(arguments, 0);
 
-    async.each(selectors,
-        function(selector, cb) {
-            client.element.call(client.client, 'css selector', selector, function(result) {
-                var value;
-
-                if (result.status == 0) {
-                    value = result.value.ELEMENT;
-                }
-
-                if (!value) {
-                    missing.push(selector);
-                }
-
-                cb();
-            });
-        },
-        function(err) {
-            if (err) {
-                client.info('An error occurred while verifying selectors');
-            } else {
-                client.assert(!missing.length, 'The following selectors are missing: ' + missing.join(', '));
-            }
-
-        });
+    async.forEach(selectors, function(s){
+        client.verify.elementPresent(s, s + ' is present');
+    })
 
     return this;
 };
