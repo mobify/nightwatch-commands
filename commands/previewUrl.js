b66f0e4e84ab4643028eb8c9c3b844ec079c9c1c
Keith Bingman <keith@mobify.com>
2014-02-18 18:14:18 -0800

    new previewUrl command and some waitForMobified work

--- commands/previewUrl.js
+++ commands/previewUrl.js
@@ -2,20 +2,22 @@ exports.command = function(siteUrl, bundleUrl, callback) {
     var client = this;
     var bundleUrl = bundleUrl || 'http://localhost:8080';
 
-    console.log('Previewing ' + siteUrl + 'using ' + bundleUrl);
+    console.log('Previewing ' + siteUrl + ' using ' + bundleUrl);
 
     return client.url('http://preview.mobify.com')
         .verify.elementPresent('#id_url')
-            .setValue('#id_url', siteUrl, function(){
-                this.clearValue('#id_site_folder', function(){
-                    this.setValue('#id_site_folder', bundleUrl, function(){
-                        this.click('#authorize', function(){
+        .setValue('#id_url', siteUrl, function(){
+            this.clearValue('#id_site_folder', function(){
+                this.setValue('#id_site_folder', bundleUrl, function(){
+                    this.click('#authorize').pause(5000, function(){
+                        client.waitForPageToBeMobified(function(result){
                             if (typeof callback === 'function') {
-                                callback.call(client, result.value);
+                                callback.call(client);
                             }
                         });
                     });
-                })
+                });
             });
+        });
 
 };
--- commands/waitForPageToBeMobified.js
+++ commands/waitForPageToBeMobified.js
@@ -1,28 +1,49 @@
-exports.command = function(callback) {
+exports.command = function(timeout, callback) {
     var client = this;
     var start = new Date().getTime();
+    var timeout = timeout || 10000;
 
-    // Uses the browser.execute to run code within the client browser,
-    // access the Mobify object and test the template.
-    return client.execute_async(
-        // gets the Mobify evaluatedDate object
-        function() { return Mobify.evaluatedData; },
-        [],
-        function(result) {
-            var now = new Date().getTime();
+    if (arguments.length === 1 && arguments[0] === 'function') {
+        callback = arguments[0];
+    }
+
+    // Attempts to get Mobify.evaluatedData. If null, it attempts again
+    // until the timeout is reached.
+    function returnEvaluatedData(result) {
+        var now = new Date().getTime();
+
+        if (result.status === 0 && result.value){
             var msg = 'Page was Mobified after ' +
                 (now - start) + ' milliseconds.';
 
-            console.log(msg);
+            // Returns the timing message
+            client.assertion(true, !!result.value, false, msg, true);
 
             // Let the DOM settle down after Mobify has done its stuff.
             client.pause(2000, function(){
                 if (typeof callback === 'function') {
-                    callback.call(client, result);
+                    callback.call(client, result.value);
                 }
             });
-
+        } else if (now - start < timeout) {
+            setTimeout(function(){
+                console.log('Waiting for client Mobify object.')
+                getEvaluatedData();
+            }, 500)
+        } else {
+            var msg = "Timed out while waiting for page to be Mobified after "
+                + timeout + " milliseconds.";
         }
-    );
+    }
 
+    function getEvaluatedData(){
+        return client.execute(
+            // gets the Mobify evaluatedData object
+            function() { return Mobify.evaluatedData; },
+            [],
+            returnEvaluatedData
+        );
+    }
+
+    return getEvaluatedData();
 };
