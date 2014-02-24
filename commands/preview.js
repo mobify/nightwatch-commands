313dc5d7c14ec0b58de694746477ac71ee0ff9da
Steve Calvert <scalvert@mobify.com>
2014-02-24 10:44:11 -0800

    Changing preview command so that we can configure the bundleUrl and siteUrl in a json file.

--- commands/preview.js
+++ commands/preview.js
@@ -1,23 +1,23 @@
-exports.command = function(siteUrl, bundleUrl, callback) {
-    var client = this;
-    var bundleUrl = bundleUrl || 'http://localhost:8080';
+var path = require('path');
+var site = require(path.join(path.resolve('./'), '/site.json'));
 
-    console.log('Previewing ' + siteUrl + ' using ' + bundleUrl);
+exports.command = function(callback) {
+  var client = this;
+  var bundleUrl = site.bundleUrl || 'http://localhost:8080';
 
-    return client.url('http://preview.mobify.com')
-        .waitForElementPresent('#id_url', 10000)
-        .setValue('#id_url', siteUrl, function(){
-            this.clearValue('#id_site_folder', function(){
-                this.setValue('#id_site_folder', bundleUrl, function(){
-                    this.click('#authorize', function(){
-                        client.waitForPageToBeMobified(10000, function(result){
-                            if (typeof callback === 'function') {
-                                callback.call(client);
-                            }
-                        });
-                    });
-                });
+  return client.url('http://preview.mobify.com')
+    .waitForElementPresent('#id_url', 10000)
+    .setValue('#id_url', site.siteUrl, function() {
+      this.clearValue('#id_site_folder', function() {
+        this.setValue('#id_site_folder', bundleUrl, function() {
+          this.click('#authorize', function() {
+            client.waitForPageToBeMobified(10000, function(result) {
+              if (typeof callback === 'function') {
+                callback.call(client, result);
+              }
             });
+          });
         });
-
+      });
+    });
 };
