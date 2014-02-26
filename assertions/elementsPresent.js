acffdcbd60e6985d865a22a6ed33076ab1b32c14
Keith Bingman <keith@mobify.com>
2014-02-26 13:48:12 -0800

    changed elementsPresent to use event emitter

--- assertions/elementsPresent.js
+++ assertions/elementsPresent.js
@@ -1,35 +1,64 @@
-var async = require('async');
+var util = require('util'),
+    events = require('events');
 
-function Assertion() {}
-Assertion.prototype.command = function command() {
-    var self = this,
-        missing = [];
+function Assertion() {
+  events.EventEmitter.call(this);
+  this.startTimer = null;
+  this.cb = null;
+  this.ms = null;
+  this.abortOnFailure = true;
+  this.selector = null;
+}
 
-    var selectors = Array.prototype.slice.call(arguments, 0);
+util.inherits(Assertion, events.EventEmitter);
 
-    async.each(selectors,
-        function(selector, cb) {
-            self.client.element.call(self, 'css selector', selector, function(result) {
-                var value;
+Assertion.prototype.command = function(selectors, callbackOrAbort) {
+  var lastArgument = Array.prototype.slice.call(arguments, 0).pop();
+  if (typeof (lastArgument) === 'function') {
+    callback = lastArgument;
+  }
 
-                if (result.status == 0) {
-                    value = result.value.ELEMENT;
-                }
+  this.startTimer = new Date().getTime();
 
-                if (!value) {
-                    missing.push(selector);
-                }
+  if (typeof arguments[2] == 'boolean') {
+    this.abortOnFailure = arguments[2];
+    this.cb = arguments[3] || function() {};
+  } else {
+    this.cb = callbackOrAbort || function() {};
+  }
 
-                cb();
-            });
-        },
-        function(err) {
-            if (!err) {
-                self.client.assertion(!missing.length, missing.join(', ') + ' missing from page.', 'all elements found', 'Locating required elements on page.', self.abortOnFailure);
-            }
-        });
+  this.selectors = Array.prototype.slice.call(arguments, 0);
+  this.checkElements();
+  return this;
+};
 
-    return this.client;
+Assertion.prototype.checkElements = function() {
+    var self = this;
+    var missing = [];
+    var selector = this.selectors.shift();
+
+    this.client.element.call(self, 'css selector', selector, function(result) {
+        var value;
+
+        if (result.status == 0) {
+            value = result.value.ELEMENT;
+        }
+
+        if (!value) {
+            missing.push(selector);
+        }
+
+        if (self.selectors.length){
+            self.checkElements();
+        } else {
+          var errorMsg = missing.join(', ') + ' missing from page.';
+          var successMsg = 'Locating required elements on page.';
+
+          self.client.assertion(!missing.length, errorMsg, 'all elements found', successMsg, false);
+          self.emit('complete');
+        }
+    });
+
 };
 
 module.exports = Assertion;
