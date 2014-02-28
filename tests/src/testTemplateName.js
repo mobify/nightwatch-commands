23a84c3a8d93a4842d30bc7191bad906a134d7c7
Keith Bingman <keith@mobify.com>
2014-02-28 10:08:51 -0800

    formatting

--- tests/src/testTemplateName.js
+++ tests/src/testTemplateName.js
@@ -1,39 +1,69 @@
 module.exports = {
-  setUp: function (callback) {
-    callback();
-  },
+    setUp: function (callback) {
+        callback();
+    },
 
-  'Mobify template name is correct' : function(test) {
-    var Assertion = require('../../assertions/templateName.js');
-    var client = {
-      getEvaluatedData : function(callback) {
-        callback({
-          value : {
-            content: {
-              templateName: 'home'
+    'Mobify template name is correct' : function(test) {
+        var Assertion = require('../../assertions/templateName.js');
+        var client = {
+            getEvaluatedData : function(callback) {
+                callback({
+                    value : {
+                        content: {
+                            templateName: 'home'
+                        }
+                    }
+                });
+            },
+            assertion : function(passed, result, expected, msg, abortOnFailure) {
+                test.equals(passed, true);
+                test.equals(result.content.templateName, 'home');
+                test.equals(expected, 'home');
+                test.equals(msg, 'Testing if the page template equals <home>.');
+                test.equals(abortOnFailure, true);
+                delete Assertion;
+                test.done();
             }
-          }
-        });
-      },
-      assertion : function(passed, result, expected, msg, abortOnFailure) {
-        test.equals(passed, true);
-        test.equals(result.content.templateName, 'home');
-        test.equals(expected, 'home');
-        test.equals(msg, 'Testing if the page template equals <home>.');
-        test.equals(abortOnFailure, true);
-        delete Assertion;
-        test.done();
-      }
-    };
+        };
 
-    var m = new Assertion();
-    m.abortOnFailure = true;
-    m.client = client;
+        var m = new Assertion();
+        m.abortOnFailure = true;
+        m.client = client;
 
-    m.command('home');
-  },
+        m.command('home');
+    },
 
-  tearDown : function(callback) {
-    callback();
-  }
+    'Mobify template name is incorrect' : function(test) {
+        var Assertion = require('../../assertions/templateName.js');
+        var client = {
+            getEvaluatedData : function(callback) {
+                callback({
+                    value : {
+                        content: {
+                            templateName: 'main'
+                        }
+                    }
+                });
+            },
+            assertion : function(passed, result, expected, msg, abortOnFailure) {
+                test.equals(passed, false);
+                test.equals(result.content.templateName, 'main');
+                test.equals(expected, 'home');
+                test.equals(msg, 'Testing if the page template equals <home>.');
+                test.equals(abortOnFailure, true);
+                delete Assertion;
+                test.done();
+            }
+        };
+
+        var m = new Assertion();
+        m.abortOnFailure = true;
+        m.client = client;
+
+        m.command('home');
+    },
+
+    tearDown : function(callback) {
+        callback();
+    }
 }
