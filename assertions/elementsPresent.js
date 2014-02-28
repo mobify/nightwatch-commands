b7852160a9823d2ea2c4a7fc5411b2895a8657d8
Keith Bingman <keith@mobify.com>
2014-02-28 10:32:31 -0800

    better messaging

--- assertions/elementsPresent.js
+++ assertions/elementsPresent.js
@@ -2,32 +2,33 @@ var util = require('util'),
     events = require('events');
 
 function Assertion() {
-  events.EventEmitter.call(this);
-  this.startTimer = null;
-  this.cb = null;
-  this.ms = null;
-  this.abortOnFailure = true;
-  this.selector = null;
+    events.EventEmitter.call(this);
+    this.startTimer = null;
+    this.cb = null;
+    this.ms = null;
+    this.abortOnFailure = true;
+    this.selector = null;
 }
 
 util.inherits(Assertion, events.EventEmitter);
 
 Assertion.prototype.command = function(selectors, callback) {
-  var args = Array.prototype.slice.call(arguments, 0);
-  var lastArgument = args[args.length - 1];
+    var args = Array.prototype.slice.call(arguments, 0);
+    var lastArgument = args[args.length - 1];
 
-  if (typeof (lastArgument) === 'function') {
-    callback = args.pop();
-  } else {
-    callback = function() {};
-  }
+    if (typeof (lastArgument) === 'function') {
+        callback = args.pop();
+    } else {
+        callback = function() {};
+    }
 
-  this.startTimer = new Date().getTime();
-  this.cb = callback;
-  this.selectors = args.slice(0);
-  this.missing = [];
-  this.checkElements();
-  return this;
+    this.startTimer = new Date().getTime();
+    this.cb = callback;
+    this.selectors = args.slice(0);
+    this.missing = [];
+    this.found = [];
+    this.checkElements();
+    return this;
 };
 
 Assertion.prototype.checkElements = function() {
@@ -37,33 +38,39 @@ Assertion.prototype.checkElements = function() {
     this.client.element.call(self, 'css selector', selector, function(result) {
         var value;
 
-        if (result.status == 0) {
+        if (result.status === 0) {
             value = result.value.ELEMENT;
         }
 
-        if (!value) {
+        if (value) {
+            self.found.push(selector);
+        } else {
             self.missing.push(selector);
         }
 
         if (self.selectors.length){
             self.checkElements();
-        } else {
-            var errorMsg = self.missing.join(', ') + ' missing from page.';
-            if (self.missing.length === 0) {
-              var msg = 'Located all required elements on page.';
-              var result = true;
-            } else {
-              var missing = self.missing.map(function(el){
-                return '<' + el + '>';
-              })
-              var msg = missing.join(', ') + ' missing from page.';
-              var result = false;
-            }
-
-            self.client.assertion(true, result, false, msg, false);
-            self.cb(result);
-            self.emit('complete');
+            return;
         }
+
+        if (self.missing.length === 0) {
+            var found = self.found.map(function(el){
+                return '<' + el + '>';
+            });
+            var msg = found.join(', ') + ' located on page.';
+            var result = true;
+        } else {
+            var missing = self.missing.map(function(el){
+                return '<' + el + '>';
+            });
+            var msg = missing.join(', ') + ' missing from page.';
+            var result = false;
+        }
+
+        self.client.assertion(true, result, false, msg, false);
+        self.cb(result);
+        self.emit('complete');
+
     });
 
 };
--- assertions/templateName.js
+++ assertions/templateName.js
@@ -1,13 +1,13 @@
 
 function Assertion() {}
 Assertion.prototype.command = function(expected, msg) {
-  var self = this;
+    var self = this;
 
-  return this.client.getEvaluatedData(function(result) {
-    var passed = result.value.content.templateName === expected;
-    msg = msg || ('Testing if the page template equals <' + expected + '>.');
-    self.client.assertion(passed, result.value, expected, msg, self.abortOnFailure);
-  });
+    return this.client.getEvaluatedData(function(result) {
+        var passed = result.value.content.templateName === expected;
+        msg = msg || ('Testing if the page template equals <' + expected + '>.');
+        self.client.assertion(passed, result.value, expected, msg, self.abortOnFailure);
+    });
 };
 
 module.exports = Assertion;
