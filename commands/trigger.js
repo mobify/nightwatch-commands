d7d201469184bbd2cbb0af54a834b2c936f390cb
Steve Calvert <scalvert@mobify.com>
2014-03-03 16:43:48 -0800

    Renaming triggerClick into more generic command. By default triggers click, but also can trigger other events

--- commands/trigger.js
+++ commands/trigger.js
@@ -4,16 +4,14 @@ exports.command = function(selector, callback) {
     // Selenium's click doesn't always register
     // We should expand this to bind and trigger more events
     //
-    client.execute(function(selector) {
-        (function simulateClick() {
-            var event = new MouseEvent('click', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': true
-            });
-            var el = document.querySelector(selector);
-            el.dispatchEvent(event);
-        })();
+    client.execute(function(selector, type) {
+        var event = new MouseEvent(type || 'click', {
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
+        });
+        var el = document.querySelector(selector);
+        el.dispatchEvent(event);
         return true;
     }, [selector], function(result){
         if (typeof callback === 'function') {
