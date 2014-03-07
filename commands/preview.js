63fb165dfba5826fc7ce0005d2a8989c03c71613
Keith Bingman <keith@mobify.com>
2014-03-07 11:04:32 -0800

    move waitForElement to a callback

--- commands/preview.js
+++ commands/preview.js
@@ -2,22 +2,23 @@ var path = require('path');
 var site = require(path.join(path.resolve('./'), '/tests/integration/site.json'));
 
 exports.command = function(callback) {
-  var client = this;
+  var browser = this;
   var bundleUrl = site.bundleUrl || 'http://localhost:8080';
 
-  return client.url('http://preview.mobify.com')
-    .waitForElementPresent('#id_url', 10000)
-    .setValue('#id_url', site.siteUrl, function() {
-      this.clearValue('#id_site_folder', function() {
-        this.setValue('#id_site_folder', bundleUrl, function() {
-          this.click('#authorize', function() {
-            client.waitForPageToBeMobified(10000, function(result) {
-              if (typeof callback === 'function') {
-                callback.call(client, result);
-              }
+  return browser.url('http://preview.mobify.com')
+    .waitForElementPresent('#id_url', 10000, function(){
+        browser.setValue('#id_url', site.siteUrl, function() {
+          this.clearValue('#id_site_folder', function() {
+            this.setValue('#id_site_folder', bundleUrl, function() {
+              this.click('#authorize', function() {
+                browser.waitForPageToBeMobified(10000, function(result) {
+                  if (typeof callback === 'function') {
+                    callback.call(browser, result);
+                  }
+                });
+              });
             });
           });
         });
-      });
     });
 };
