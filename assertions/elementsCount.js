283d4c2e8eeb1913ff81e98bd8b01b5a8ea69b00
Keith Bingman <keith@mobify.com>
2014-03-18 13:57:34 -0700

    fixed elementsPresent

--- assertions/elementsCount.js
+++ assertions/elementsCount.js
@@ -1,14 +1,50 @@
-function Assertion() {}
-Assertion.prototype.command = function command(cssSelector, expectedCount, message) {
-    var self = this;
-    var msg = (message && message !== '') ? message : 'Testing if elements count <' + cssSelector + '> is ' + expectedCount + '.';
+/**
+ * Checks if the given selector is present the expected number of times.
+ *
+ * ```
+ *    this.demoTest = function (client) {
+ *      browser.assert.elementsCount('#x-root', 1);
+ *    };
+ * ```
+ *
+ * @method attributeEquals
+ * @param {string} selector The selector (CSS / Xpath) used to locate the element.
+ * @param {string} attribute The attribute name
+ * @param {string} expected The expected value of the attribute to check.
+ * @param {string} [message] Optional log message to display in the output. If missing, one is displayed by default.
+ * @api assertions
+ */
 
-    return this.client.elements("css selector", cssSelector, function(result) {
-        var value = 0;
-        if (result.status == 0) {
-            value = result.value.length;
+var util = require('util');
+exports.assertion = function(selector, expected, msg) {
+
+    var MSG_ELEMENT_NOT_FOUND = 'Testing if the count for the element <%s> is %s. ' +
+        'Element could not be located.';
+
+    this.message = msg || util.format('Testing if the count for the element <%s> is %s.', selector, expected);
+
+    this.expected = function() {
+        return expected;
+    };
+
+    this.pass = function(value) {
+        return value === expected;
+    };
+
+    this.failure = function(result) {
+        var failed = result === false || result && result.status === -1;
+        if (failed) {
+            this.message = msg || util.format(MSG_ELEMENT_NOT_FOUND, selector, expected);
         }
-        self.client.assertion(value === expectedCount, value, expectedCount, msg, self.abortOnFailure);
-    });
+        return failed;
+    };
+
+    this.value = function(result) {
+        return result.value.length;
+    };
+
+    this.command = function(callback) {
+        return this.api.elements('css selector', selector, callback);
+    };
+
 };
-module.exports = Assertion;
