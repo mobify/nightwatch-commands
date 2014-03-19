77f8e6217f69e8d8e831b3011e1484de2503312f
Keith Bingman <keith@mobify.com>
2014-03-18 18:27:30 -0700

    getting template and evaluated data to work

--- commands/waitForPageToBeMobified.js
+++ commands/waitForPageToBeMobified.js
@@ -1,18 +1,22 @@
 exports.command = function(milliseconds, callback) {
     if (milliseconds && typeof milliseconds != 'number') {
       throw new Error('waitForPageToBeMobified expects first parameter to be number; ' +
-        typeof (milliseconds) + ' given')
+        typeof (milliseconds) + ' given');
     }
 
     var client = this;
     var messages = {
-      success: 'Page was Mobified after ',
-      failure: 'Timed out after '
-    }
+        success: 'Page was Mobified after ',
+        failure: 'Timed out after '
+    };
 
-    // Uses waitForCondition to run code within the client browser
-    //
-    client.waitForCondition('return Mobify.evaluatedData;', milliseconds, 2000, messages, function(result){
+    /** Uses waitForCondition to check for either the Mobify or
+     *  Adaptive object in the client browser
+     */
+    client.waitForCondition(function(){
+        var framework = Mobify || Adaptive;
+        return !!framework;
+    }, milliseconds, 2000, messages, function(result){
         if (typeof callback === 'function') {
                 callback.call(client, result);
         }
