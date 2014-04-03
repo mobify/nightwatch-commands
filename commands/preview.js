3c968f46259701abeaa6c441293e58483c83ac20
Keith Bingman <keith@mobify.com>
2014-04-03 16:41:46 -0700

    changed preview to take a URL argument

--- commands/preview.js
+++ commands/preview.js
@@ -2,11 +2,21 @@ var path = require('path');
 var site = require(path.join(path.resolve('./'), '/tests/integration/site.json'));
 var qs = require('querystring');
 
-exports.command = function(callback) {
+exports.command = function(url, callback) {
     var browser = this;
+
+    var lastArgument = Array.prototype.slice.call(arguments, 0).pop();
+    if (typeof (lastArgument) === 'function') {
+        callback = lastArgument;
+    }
+
+    if (typeof url !== 'string'){
+        url = site.siteUrl;
+    }
+
     var bundleUrl = site.bundleUrl || 'http://localhost:8080';
 
-    var params = qs.stringify({ url: site.siteUrl, site_folder: bundleUrl });
+    var params = qs.stringify({ url: url, site_folder: bundleUrl });
 
     return browser.url('http://preview.mobify.com?' + params)
         .waitForElementPresent('#authorize', 10000, function() {
