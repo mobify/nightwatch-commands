f4fafc290908325677defc38dae8233f0f215456
Keith Bingman <keith@mobify.com>
2014-04-03 16:48:33 -0700

    added some docs

--- commands/preview.js
+++ commands/preview.js
@@ -1,3 +1,30 @@
+/**
+ * Preview will use mobify.preview.com to open a website and allow you to preview
+ * a given bundle. The bundle and base URL will need to be set in the the
+ * `tests/integration/site.json` file. Additionally, you can pass a URL as an
+ * argument when you call preview(). Upon completion, waitForPageToBeMobified
+ * is called, to be sure that the adaptation is complete. 
+ *
+ * ```
+ *    this.demoTest = function (client) {
+ *      browser.preview();
+ *    };
+ * ```
+ * or with a URL
+ *
+ * ```
+ *    this.demoTest = function (client) {
+ *      browser.preview('http://my-awesome-project.com');
+ *    };
+ * ```
+ *
+ * @method attributeEquals
+ * @param {string} [URL] (optional) The URL to be previewed.
+ * @param {function} callback The function to be called on completion.
+ * @api assertions
+ */
+
+
 var path = require('path');
 var site = require(path.join(path.resolve('./'), '/tests/integration/site.json'));
 var qs = require('querystring');
