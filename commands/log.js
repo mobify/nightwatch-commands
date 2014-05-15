b89f5e67fe59342b95b1dce8d50728b63434537d
Justin Vaillancourt <justin@mobify.com>
2014-05-15 14:25:55 -0700

    Change log command to be synchronous

--- commands/log.js
+++ commands/log.js
@@ -1,6 +1,3 @@
-var util = require('util');
-var events = require('events');
-
 /**
  * Logs a message to the console, allowing for adding messages to test output
  *
@@ -14,27 +11,17 @@ var events = require('events');
  *
  * @method log
  * @param {string} message The message to log to the console.
- * @param {string} logLevel The type of log message to output.
  * @param {function} [callback] Optional callback function to be called when the command finishes.
  * @api commands
  */
-
-function Log() {
-    events.EventEmitter.call(this);
-}
-
-util.inherits(Log, events.EventEmitter);
-
-Log.prototype.command = function(message, logLevel, callback) {
+ var log = function(message, callback) {
     message && console.log(message);
 
     if (typeof callback === 'function') {
-        callback.call(this.client);
+        callback.call(this);
     }
 
-    this.emit('complete');
-
     return this;
-};
+ };
 
-module.exports = Log;
+ exports.command = log;
