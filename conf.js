00abe01157d6a7017f1e0ee91ea04cdb877e95eb
Helen <helen@mobify.com>
2014-06-16 16:49:47 -0700

    Update Nightwatch so that it will download the selenium web driver and the chrome web driver on install

---
+++ conf.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+// see http://selenium-release.storage.googleapis.com/index.html for latest
+var version = '2.42.0';
+
+module.exports = {
+  selenium: {
+    path: path.join(__dirname, 'selenium', 'selenium-server.jar'),
+    v: version
+  },
+  chromeDr: {
+    path: path.join(__dirname, 'selenium', 'drivers', 'chromedriver'),
+    // see http://chromedriver.storage.googleapis.com/index.html
+    v: '2.9'
+  }
+};
