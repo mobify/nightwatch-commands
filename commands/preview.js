97ba2cee5625e935d484c2b47799963dd3c3cbea
Ellen Wong <ellen@mobify.com>
2015-11-26 12:37:49 -0800

    Make site.json optional

--- commands/preview.js
+++ commands/preview.js
@@ -1,7 +1,8 @@
 /**
  * Preview will use preview.mobify.com to open a website and allow you to preview
  * a given bundle. The bundle and base URL will need to be set in the the
- * `tests/integration/site.json` file. Additionally, you can pass a URL as an
+ * `tests/system/site.json` file. If this file does not exist, 
+ * Additionally, you can pass a URL as an
  * argument when you call preview(). Upon completion, waitUntilMobified
  * is called, to be sure that the adaptation is complete.
  *
@@ -26,50 +27,57 @@
 
 
 var path = require('path');
-var site = require(path.join(path.resolve('./'), '/tests/system/site.json'));
+try {
+    var site = require(path.join(path.resolve('./'), '/tests/system/site.json'));
+} catch (e) {
+    if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
+        console.log('Site.json does not exist, so .preview() is not available.');
+    }
+}
 var qs = require('querystring');
 
-site = site.profiles[site.activeProfile];
+if (site) { 
+    site = site.profiles[site.activeProfile];
 
-exports.command = function(url, callback) {
-    var browser = this;
+    exports.command = function(url, callback) {
+        var browser = this;
 
-    if (typeof url === 'function') {
-        callback = url;
-        url = site.siteUrl;
-    }
+        if (typeof url === 'function') {
+            callback = url;
+            url = site.siteUrl;
+        }
 
-    // First checks for the URL, otherwise uses the site.siteURL, then makes sure
-    // that there is an http prefix. The preveiw function doesn't need this, but
-    // the browser.get() method does.
-    url = url || site.siteUrl;
+        // First checks for the URL, otherwise uses the site.siteURL, then makes sure
+        // that there is an http prefix. The preveiw function doesn't need this, but
+        // the browser.get() method does.
+        url = url || site.siteUrl;
 
-    if (!url.match(/^http/)) {
-        throw new Error('Site URL must be correctly formatted');
-    }
+        if (!url.match(/^http/)) {
+            throw new Error('Site URL must be correctly formatted');
+        }
 
-    // If the production flag is set, just runs a `get()` on the URL.
-    if (site.production) {
-        return browser.get(url, function(result) {
-            if (typeof callback === 'function') {
-                callback.call(browser, result);
-            }
-        });
-    }
+        // If the production flag is set, just runs a `get()` on the URL.
+        if (site.production) {
+            return browser.get(url, function(result) {
+                if (typeof callback === 'function') {
+                    callback.call(browser, result);
+                }
+            });
+        }
 
-    var bundleUrl = site.bundleUrl || 'http://localhost:8080';
+        var bundleUrl = site.bundleUrl || 'http://localhost:8080';
 
-    var params = qs.stringify({'url': url, 'site_folder': bundleUrl});
+        var params = qs.stringify({'url': url, 'site_folder': bundleUrl});
 
-    return browser.url('https://preview.mobify.com?' + params)
-        .waitForElementPresent('#authorize', 10000, function() {
-            this.click('#authorize', function() {
-                browser.waitUntilMobified(10000, function(result) {
-                    if (typeof callback === 'function') {
-                        callback.call(browser, result);
-                    }
+        return browser.url('https://preview.mobify.com?' + params)
+            .waitForElementPresent('#authorize', 10000, function() {
+                this.click('#authorize', function() {
+                    browser.waitUntilMobified(10000, function(result) {
+                        if (typeof callback === 'function') {
+                            callback.call(browser, result);
+                        }
+                    });
                 });
             });
-        });
-
-};
+    };
+}
