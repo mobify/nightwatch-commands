85619327e9b68b0b92630851676200af1267b3d1
Marlow Payne <marlow@mobify.com>
2016-01-12 16:45:58 -0800

    Bring up-to-date with eslint standard; Lint everything for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,37 +3,29 @@ module.exports = function(grunt) {
         'Gruntfile.js',
         'assertions/**/*.js',
         'commands/**/*.js',
-        'tests/src/*.js',
-        '!tests/nightwatch.js',
-        '!tests/run_tests.js',
+        'tests/**/*.js',
         '!tests/node_modules/**'
     ];
 
-    var excludes = [
-
-    ];
-
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jscs-checker');
+    grunt.loadNpmTasks('grunt-eslint');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            all: {
-                src: grunt.option('file') || targets,
+        eslint: {
+            dev: {
+                src: targets,
                 options: {
-                    force: true,
-                    ignores: excludes,
-                    jshintrc: 'node_modules/mobify-code-style/javascript/.jshintrc'
+                    reset: true,
+                    config: require.resolve('mobify-code-style/javascript/.eslintrc')
+                }
+            },
+            prod: {
+                src: targets,
+                options: {
+                    reset: true,
+                    config: require.resolve('mobify-code-style/javascript/.eslintrc-prod')
                 }
             }
-        },
-        jscs: {
-            options: {
-                config: 'node_modules/mobify-code-style/javascript/.jscsrc',
-                excludeFiles: excludes
-            },
-            src: targets
         }
     });
 
@@ -50,5 +42,5 @@ module.exports = function(grunt) {
         });
     });
 
-    grunt.registerTask('lint', ['jshint:all', 'jscs:src']);
+    grunt.registerTask('lint', ['eslint:prod']);
 };
