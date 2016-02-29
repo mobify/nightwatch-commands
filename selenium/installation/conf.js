2c15f059d18c6b18c792780d6f1f374929e96c8f
Derrick <derrick@mobify.com>
2016-02-29 11:19:57 -0800

    update to latest selenium and chrome drivers
    Signed-off-by: Derrick <derrick@mobify.com>

--- selenium/installation/conf.js
+++ selenium/installation/conf.js
@@ -1,10 +1,10 @@
 var path = require('path');
 
 // see http://selenium-release.storage.googleapis.com/index.html for latest
-var seleniumVersion = '2.48.2';
+var seleniumVersion = '2.52.0';
 
 // see http://chromedriver.storage.googleapis.com/index.html
-var chromeDriverVersion = '2.20';
+var chromeDriverVersion = '2.21';
 
 module.exports = {
   selenium: {
