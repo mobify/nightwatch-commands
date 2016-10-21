423b8e0c0b71b8cb25fa68bf285199417b1a6903
Ellen Wong <ellen@mobify.com>
2016-10-20 17:25:30 -0700

    Use selenium-download

---
+++ selenium/install.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var selenium = require('selenium-download');
+
+// Download Selenium server and Chromedriver
+selenium.ensure(__dirname, function(error) {
+    if (error) {
+        console.error(error.stack);
+    } else {
+    	// Rename so that we don't need to update paths in project configs
+        var oldSeleniumPath = path.join(__dirname, 'selenium.jar');
+        var newSeleniumPath = path.join(__dirname, 'selenium-server.jar');
+        
+        fs.rename(oldSeleniumPath, newSeleniumPath, function(error) {
+            if (error) console.log(error);
+            return;
+        });
+
+        mkdirp(path.join(__dirname, 'drivers'));
+        var oldChromedriverPath = path.join(__dirname, 'chromedriver');
+        var newChromedriverPath = path.join(__dirname, 'drivers', 'chromedriver');
+
+        fs.rename(oldChromedriverPath, newChromedriverPath, function(error) {
+            if (error) console.log(error);
+            return;
+        });
+    }
+});
