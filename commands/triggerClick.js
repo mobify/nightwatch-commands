18dcf3acbe39869d969b595789a555c3954dd5a7
Ellen Wong <ellen@mobify.com>
2017-06-02 16:11:29 -0700

    Clean up and update CHANGELOG

--- commands/triggerClick.js
+++ commands/triggerClick.js
@@ -2,8 +2,8 @@ exports.command = function(selector, callback) {
     var client = this;
     /*
     / Use Javascript's click when Selenium's does not register.
-    */ 
-    client.execute('document.querySelector("' + selector + '").click();', function (result) {
+    */
+    client.execute('document.querySelector("' + selector + '").click();', function(result) {
         if (typeof callback === 'function') {
             callback.call(client, result);
         }
