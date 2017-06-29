8955193c008893b904f39c62d98b8bd346fe0985
Ellen Wong <ellen@mobify.com>
2017-06-29 15:34:53 -0700

    Preview can take URL, bundle, isProduction args

--- commands/preview.js
+++ commands/preview.js
@@ -1,39 +1,45 @@
 /**
  * Preview will use preview.mobify.com to open a website and allow you to preview
- * a given bundle. The bundle and base URL will need to be set in the the
- * `tests/system/site.json` file. Additionally, you can pass a URL as an
- * argument when you call preview(). Upon completion, waitUntilMobified
- * is called, to be sure that the adaptation is complete.
+ * a given bundle. The site URL and bundle URL should be passed in along with a 
+ * production flag. Legacy projects use a `tests/system/site.js` file to set the site URL * and bundle URL. Upon completion, waitUntilMobified is called, to be sure that the 
+ * adaptation is complete.
 *
- * If `site.json` does not exist, this command will just go to the specified URL.
+ * Usage:
+ * ```
+ *    this.demoTest = function (client) {
+ *      browser.preview('https://www.merlinspotions.com', 'https://localhost:8443/loader.js', false);
+ *    };
+ * ```
+ * 
+ * With a site.js file (deprecated):
  *
  * ```
  *    this.demoTest = function (client) {
  *      browser.preview();
  *    };
  * ```
- * or with a URL
- *
+ * Or with a site.js file and a URL (deprecated): 
  * ```
  *    this.demoTest = function (client) {
  *      browser.preview('http://my-awesome-project.com');
  *    };
  * ```
  *
- * @method attributeEquals
- * @param {string} [URL] (optional) The URL to be previewed.
- * @param {function} callback The function to be called on completion.
- * @api assertions
+ * @method preview
+ * @param {string} [url] Corresponds to the Site URL field on https://preview.mobify.com
+ * @param {string} [bundle] Corresponds to the Bundle Location field on https://preview.mobify.com
+ * @param {boolean} [isProduction] true if the test should bypass Preview step and test with the published bundle, false if the test should use the specified bundle
+ * @param {function} [callback] Optional callback function to be called when the command finishes.
+ * @api commands
  */
 
 var path = require('path');
 var qs = require('querystring');
+var siteConfig, site;
 
-exports.command = function(url, callback) {
-    var browser = this;
-
+var readSiteConfig = function() {
     try {
-        var siteConfig = require(path.join(path.resolve('./'), '/tests/system/site.json'));
+        siteConfig = require(path.join(path.resolve('./'), '/tests/system/site.json'));
     } catch (e) {
         if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
             console.log('Not using optional site.json. Looking for site.js...');
@@ -41,7 +47,7 @@ exports.command = function(url, callback) {
     }
 
     try {
-        var siteConfig = require(path.join(path.resolve('./'), '/tests/system/site.js'));
+        siteConfig = require(path.join(path.resolve('./'), '/tests/system/site.js'));
     } catch (e) {
         if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
             console.log('Not using optional /tests/system/site.js.');
@@ -49,7 +55,7 @@ exports.command = function(url, callback) {
     }
 
     try {
-        var siteConfig = require(path.join(path.resolve('./'), '/system/site.js'));
+        siteConfig = require(path.join(path.resolve('./'), '/system/site.js'));
     } catch (e) {
         if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
             console.log('Not using optional /system/site.js.');
@@ -57,50 +63,61 @@ exports.command = function(url, callback) {
     }
 
     if (siteConfig) {
-        var site = siteConfig.profiles[siteConfig.activeProfile];
+        site = siteConfig.profiles[siteConfig.activeProfile];
+    }
 
+};
+
+exports.command = function(url, bundle, isProduction, callback) {
+    var browser = this;
+    if (arguments.length <= 2) { // Legacy command, uses site.js
         if (typeof url === 'function') {
             callback = url;
-            url = site.siteUrl;
+            url = '';
+        } else if (typeof bundle === 'function') {
+            callback = bundle;
+            bundle = '';
         }
-
-        // First checks for the URL, otherwise uses the site.siteURL, then makes sure
-        // that there is an http prefix. The preview function doesn't need this, but
-        // the browser.get() method does.
-        url = url || site.siteUrl;
-
-        if (!url.match(/^http/)) {
-            throw new Error('Site URL must be correctly formatted');
-        }
-
-        // If the production flag is set, just runs a `get()` on the URL.
-        if (site.production) {
-            return browser.get(url, function(result) {
-                if (typeof callback === 'function') {
-                    callback.call(browser, result);
-                }
-            });
-        }
-
-        var bundleUrl = site.bundleUrl || 'https://localhost:8443/adaptive.js';
-
-        var params = qs.stringify({'url': url, 'site_folder': bundleUrl});
-
-        return browser.url('https://preview.mobify.com?' + params)
-            .waitForElementPresent('#authorize', 10000, function() {
-                this.click('#authorize', function() {
-                    browser.waitUntilMobified(10000, function(result) {
-                        if (typeof callback === 'function') {
-                            callback.call(browser, result);
-                        }
-                    });
-                });
-            });
+        readSiteConfig();
     } else {
-        return browser.url(url, function(result) {
+        site = new Object();
+        site.siteUrl = url;
+        site.bundleUrl = bundle;
+        if (typeof isProduction === 'function') {
+            callback = isProduction;
+            isProduction = '';
+        }
+    }
+    // First checks for the URL, otherwise uses the site.siteURL, then makes sure
+    // that there is an http prefix. The preview function doesn't need this, but
+    // the browser.get() method does.
+    url = url || site.siteUrl;
+
+    if (!url.match(/^http/)) {
+        throw new Error('Site URL must begin with HTTP(S)');
+    }
+
+    // If the production flag is set, just runs a `get()` on the URL.
+    if (site.production || isProduction) {
+        return browser.get(url, function(result) {
             if (typeof callback === 'function') {
                 callback.call(browser, result);
             }
         });
     }
+
+    var bundleUrl = site.bundleUrl || 'https://localhost:8443/loader.js';
+
+    var params = qs.stringify({'url': url, 'site_folder': bundleUrl});
+
+    return browser.url('https://preview.mobify.com?' + params)
+        .waitForElementPresent('#authorize', 10000, function() {
+            this.click('#authorize', function() {
+                browser.waitUntilMobified(10000, function(result) {
+                    if (typeof callback === 'function') {
+                        callback.call(browser, result);
+                    }
+                });
+            });
+        });
 };
